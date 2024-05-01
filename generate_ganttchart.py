45a91c6af5a2faa009508754f1529739137a6fc0
t.uehara <t.uehara@gmail.com>
2024-05-01 11:11:13 +0900

    add get_date to check github actions datetime

--- generate_ganttchart.py
+++ generate_ganttchart.py
@@ -7,6 +7,8 @@ bgcolor = (255,255,255)
 gchart = elegantt.EleGantt( chartsize, bgcolor)
 gchart.set_font("/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc")
 
+print(gchart.get_today())
+
 gchart.set_max_day(28)
 
 gchart.draw_calendar()
@@ -15,8 +17,8 @@ with open("index.md") as file:
     for line in file:
         if line[0] == "|":
             line = line.split("|")
-            print(line)
             if re.match(r'^\d{4}-\d{2}-\d{2}$',line[1]):
+                print(line)
                 gchart.draw_campain(line[1],line[2],line[3])
 
 gchart.save("ganttchart.png")
