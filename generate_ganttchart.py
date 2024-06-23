9550a0e9f785121497dca5036c49641648af0b36
t.uehara <t.uehara@gmail.com>
2024-06-23 10:44:50 +0900

    カレンダーの表示幅を21日に指定

--- generate_ganttchart.py
+++ generate_ganttchart.py
@@ -6,25 +6,25 @@ today = (
     + datetime.timedelta(hours=15)
     ).strftime('%Y-%m-%d')
 
-gchart = elegantt.EleGantt(today=today, firstday=today)
-gchart.set_font(
+g = elegantt.EleGantt(today=today, firstday=today)
+g.set_font(
     regular="/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc",
     bold="/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc")
-gchart.set_max_day(21)
+g.set_max_day(21)
+g.day_num = 21
 with open("index.md") as file:
     content = file.read()
-    events = gchart.parse_markdown(content)
-    gchart.resize(size=(
-        692,
-        60 + len(events)*45 + 10
-    ))
-    gchart.set_max_day(21)
-    gchart.draw_calendar()
+    events = g.parse_markdown(content)
+    g.resize(size=(
+        g.calc_width(21),
+        g.calc_height(len(events))
+    ), today=today, firstday=today)
+    g.draw_calendar()
     for event in events:
         print(event)
-        gchart.draw_campain(
+        g.draw_campain(
             event["start"].strftime('%Y-%m-%d'),
             event["end"].strftime('%Y-%m-%d'),
             event["title"]
         )
-gchart.save("ganttchart.png")
+g.save("ganttchart.png")
